641f1a7383f98c233de8ae56dae30dbeb7a87c3c
Wannes Gennar <wannes.gennar@gmail.com>
2016-08-08 14:01:54 +0200

    chore(karma): change reporter
    Change the reporter to story and raise log level to 'LOG_WARNING'

--- karma.conf.js
+++ karma.conf.js
@@ -42,7 +42,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['story'],
 
 
     // web server port
@@ -55,7 +55,7 @@ module.exports = function(config) {
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_WARN,
 
 
     // enable / disable watching file and executing tests whenever any file changes
