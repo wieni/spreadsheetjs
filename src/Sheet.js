a3cd6cef4b3f71570e270654aaaa069117a60ca9
Wannes Gennar <wannes.gennar@gmail.com>
2016-08-09 17:37:49 +0200

    feat: update classes
    Update the classes with a major overhaul

--- src/Sheet.js
+++ src/Sheet.js
@@ -1,8 +1,6 @@
-import { missing, assert } from 'utils';
-
 /**
  * Wrapper class that provides a simpeler interface to working with sheets
- * in the Google spreadsheet API
+ * in the Google spreadsheet API.
  */
 export default class Sheet {
 	/**
@@ -10,7 +8,7 @@ export default class Sheet {
 	 * @param  {string} name (required) The name of the spreadsheet.
 	 * @param  {Object} data The raw data retrieved from the API.
 	 */
-	constructor(name = missing`sheet name`, data) {
+	constructor(name, data) {
 		this.name = name;
 		this.data = [];
 		this.parse(data);
@@ -18,31 +16,97 @@ export default class Sheet {
 
 	/**
 	 * Parse the raw data retrieved from the API
-	 * @param  {Object} data (required) The raw data retrieved from the API
+	 * @param  {Object} data (required) The raw data retrieved from the API.
 	 * @param  {string} data.values The values of the rows.
 	 * @param  {string} data.majorDimension The format in which the API returned the raw data.
 	 * @return {Sheet} returns itself for chaining.
 	 */
-	parse(data = missing`sheet data`) {
-		assert(data.values !== (void 0));
-		assert(data.majorDimension === 'ROWS');
+	parse(data) {
 		this.data = data.values;
 	}
 
 	/**
 	 * Read a cell from the sheet.
 	 * @param  {string} coord The coordinate of the cell to read in the A1 notation.
-	 * @return {string} Returns the data at the requested coordinate
+	 * @return {string} Returns the data at the requested coordinate,
 	 * or undefined if invalid coordinates were given.
 	 */
-	read(coord = missing`read position`) {
+	read(coord) {
 		const [x, y] = this.__coordinates(coord);
 
-		if (x < this.data.length || y < this.data[x].length) {
+		if (x >= this.data.length || y >= this.data[x].length) {
 			return (void 0);
 		}
 
-		return this.data[x][y];
+		return this.data[y][x];
+	}
+
+	/**
+	 * Read a range of coordinates.
+	 * @param  {string} start The begin coordinates in A1 notation.
+	 * @param  {string} end The end coordinates in A1 notation.
+	 * @return {Array} An array with values if a single row was requested, a two dimensional array is more rows were requested.
+	 */
+	range(start, end) {
+		const [startX, startY] = this.__coordinates(start);
+		const [endX, endY] = this.__coordinates(end);
+
+		const result = [];
+		if (startY === endY) {
+			for (let i = startX; i <= endX; i++) {
+				result.push(this.data[startY][i]);
+			}
+		}
+		else if (startX === startY) {
+			for (let i = startY; i <= endY; i++) {
+				result.push(this.data[i][startX]);
+			}
+		} else {
+			for (let i = startY; i <= endY; i++) {
+				let row = [];
+				for (let j = startX; j <= endX; j++) {
+					row.push(this.data[i][j] || '');
+				}
+				result.push(row);
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * Reads a column from the sheet.
+	 * @param  {string} which The column to read (eg 'A', 'B', ...).
+	 * @param  {string} [start='1'] The row to start reading from (1, 2, ...).
+	 * @return {Array}  returns a flat array that represents the requested column.
+	 */
+	column(which, start = '1') {
+		const [x, y] = this.__coordinates(which + start);
+		const results = [];
+
+		this.data.forEach((row, index) => {
+			if (index >= y) {
+				results.push(row.length <= x ? '' : row[x]); // empty rows are empty arrays.
+			}
+		});
+
+		return results;
+	}
+
+	/**
+	 * Read a row from the sheet.
+	 * @param  {string} which The row to read.
+	 * @param  {string} start The column to start from (defaults to the first column 'A').
+	 * @return {Array}  returns a flat array that represents the requested row.
+	 */
+	row(which, start = 'A') {
+		const [x, y] = this.__coordinates(start + which);
+
+		if (y >= this.data.length) {
+			return [];
+		}
+
+		return this.data[y].filter((_, index) => (x <= index));
 	}
 
 	/**
@@ -53,10 +117,13 @@ export default class Sheet {
 	 * and the Y property pointing to the Y coordinate.
 	 */
 	__coordinates(coord) {
-		assert(coord.match(/[a-zA-Z][0-9]+/), 'Invalid coordinates given.');
-		const x = coord.charAt().toUpperCase().charCodeAt() - 65; // 65 = 'A'
-		const y = parseInt(coord.substr(1), 10);
+		if ( ! coord.match(/[a-zA-Z][0-9]+/) ) {
+			throw new Error(`Invalid coordinates given (${coord}).`);
+		}
 
-		return { x, y };
+		const x = coord.charAt().toUpperCase().charCodeAt() - 65; // 65 = 'A'
+		const y = parseInt(coord.substr(1), 10) - 1; // normalize 1 to 0
+
+		return [x, y];
 	}
 }
--- src/Spreadsheet.js
+++ src/Spreadsheet.js
@@ -1,4 +1,6 @@
-import { missing, get } from 'utils';
+/* global API_KEY b:true */
+import $ from 'jquery';
+import Sheet from 'api/Sheet';
 
 /**
  * A simple wrapper class that exposes the functionality of the Google spreadsheet API (v4).
@@ -9,7 +11,7 @@ export default class Spreadsheet {
 	 * @param  {string} key Your Google API key.
 	 * @param  {string} hash Your Spreadsheet hash.
 	 */
-	constructor(key = missing`API key`, hash = missing`Spreadsheet hash`) {
+	constructor(hash, key = API_KEY) {
 		this.key = key;
 		this.hash = hash;
 		this.ready = false;
@@ -17,27 +19,51 @@ export default class Spreadsheet {
 		this.locale = null;
 		this.sheets = [];
 		this.timeZone = null;
+		this.__cache = new Map();
+		this.__parsing = new Promise((resolve, reject) => {
+			$.get(`https://sheets.googleapis.com/v4/spreadsheets/${this.hash}?key=${this.key}`)
+				.done(this.__parse.bind(this, resolve))
+				.fail(reject);
+		});
 	}
 
 	/**
-	 * Make GET request to retrieve meta data about the spreadsheet.
-	 * @return {Spreadsheet} returns itself for chaining.
+	 * Wait for the current long pending task to finish.
+	 * @return {Promise} The promise that resolves when the Spreadsheet is done loading.
 	 */
-	init() {
-		const promise = get(`https://sheets.googleapis.com/v4/spreadsheets/${this.hash}?key=${this.key}`);
-		promise.then(this.__parse)
-		.catch((status, response) => {
-			throw new Error(`Server responded with ${status}: ${JSON.stringify(response)}`);
+	wait() {
+		return this.__parsing;
+	}
+
+	/**
+	 * Load a sheet from the spreadsheet.
+	 * @param  {string} name The name of the sheet to request (if left empty, the first sheet is taken).
+	 * @return {Promise}                       [description]
+	 */
+	sheet(name = this.sheets[0]) {
+		if (this.__cache.has(name)) {
+			return this.__cache.get(name);
+		}
+		const promise = new Promise((resolve, reject) => {
+			$.get(`https://sheets.googleapis.com/v4/spreadsheets/${this.hash}/values/${name}?key=${this.key}`)
+				.done((data) => {
+					const sheet = new Sheet(name, data);
+					resolve(sheet);
+				})
+				.fail(reject);
 		});
 
-		return this;
+		this.__cache.set(name, promise);
+		return promise;
 	}
 
 	/**
 	 * Parse the raw data from the API.
+	 * @private
+	 * @param  {callback} The resolve callback (internally used).
 	 * @param  {Object} raw The raw data returned by the API.
 	 */
-	__parse(raw) {
+	__parse(resolve, raw) {
 		this.title = raw.properties.title;
 		this.locale = raw.properties.locale;
 		this.timeZone = raw.properties.timeZone;
@@ -45,5 +71,7 @@ export default class Spreadsheet {
 		raw.sheets.forEach((sheet) => this.sheets.push(sheet.properties.title));
 
 		this.ready = true;
+
+		resolve(this);
 	}
 }
