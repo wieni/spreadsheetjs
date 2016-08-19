f3d56f2113d8465ddd5dcdf4af4dfa8e41cceafa
Wannes Gennar <wannes.gennar@gmail.com>
2016-08-19 11:58:29 +0200

    feat(sheet,spreadsheet): add rows method
    Add the rows method and various bugfixes

--- src/Sheet.js
+++ src/Sheet.js
@@ -1,36 +1,39 @@
 /**
- * Wrapper class that provides a simpeler interface to working with sheets
- * in the Google spreadsheet API.
- */
+* Wrapper class that provides a simpeler interface to working with sheets
+* in the Google spreadsheet API.
+*/
 export default class Sheet {
 	/**
-	 * Create a new sheet instance.
-	 * @param  {string} name (required) The name of the spreadsheet.
-	 * @param  {Object} data The raw data retrieved from the API.
-	 */
-	constructor(name, data) {
+	* Create a new sheet instance.
+	* @param  {string} name (required) The name of the spreadsheet.
+	* @param  {Object} data The raw data retrieved from the API.
+	*/
+	constructor(name, data = null) {
 		this.name = name;
 		this.data = [];
-		this.parse(data);
+
+		if (data !== null) {
+			this.parse(data);
+		}
 	}
 
 	/**
-	 * Parse the raw data retrieved from the API
-	 * @param  {Object} data (required) The raw data retrieved from the API.
-	 * @param  {string} data.values The values of the rows.
-	 * @param  {string} data.majorDimension The format in which the API returned the raw data.
-	 * @return {Sheet} returns itself for chaining.
-	 */
+	* Parse the raw data retrieved from the API
+	* @param  {Object} data (required) The raw data retrieved from the API.
+	* @param  {string} data.values The values of the rows.
+	* @param  {string} data.majorDimension The format in which the API returned the raw data.
+	* @return {Sheet} returns itself for chaining.
+	*/
 	parse(data) {
 		this.data = data.values;
 	}
 
 	/**
-	 * Read a cell from the sheet.
-	 * @param  {string} coord The coordinate of the cell to read in the A1 notation.
-	 * @return {string} Returns the data at the requested coordinate,
-	 * or undefined if invalid coordinates were given.
-	 */
+	* Read a cell from the sheet.
+	* @param  {string} coord The coordinate of the cell to read in the A1 notation.
+	* @return {string} Returns the data at the requested coordinate,
+	* or undefined if invalid coordinates were given.
+	*/
 	read(coord) {
 		const [x, y] = this.__coordinates(coord);
 
@@ -42,14 +45,18 @@ export default class Sheet {
 	}
 
 	/**
-	 * Read a range of coordinates.
-	 * @param  {string} start The begin coordinates in A1 notation.
-	 * @param  {string} end The end coordinates in A1 notation.
-	 * @return {Array} An array with values if a single row was requested, a two dimensional array is more rows were requested.
-	 */
+	* Read a range of coordinates.
+	* @param  {string} start The begin coordinates in A1 notation.
+	* @param  {string} end The end coordinates in A1 notation.
+	* @return {Array} An array with values if a single row was requested, a two dimensional array is more rows were requested.
+	*/
 	range(start, end) {
 		const [startX, startY] = this.__coordinates(start);
 		const [endX, endY] = this.__coordinates(end);
+
+		if (startY >= this.data.length || startX >= this.data[startY].length || endY >= this.data.length || endX >= this.data[endY].length) {
+			return [];
+		}
 
 		const result = [];
 		if (startY === endY) {
@@ -57,7 +64,7 @@ export default class Sheet {
 				result.push(this.data[startY][i]);
 			}
 		}
-		else if (startX === startY) {
+		else if (startX === endX) {
 			for (let i = startY; i <= endY; i++) {
 				result.push(this.data[i][startX]);
 			}
@@ -75,11 +82,11 @@ export default class Sheet {
 	}
 
 	/**
-	 * Reads a column from the sheet.
-	 * @param  {string} which The column to read (eg 'A', 'B', ...).
-	 * @param  {string} [start='1'] The row to start reading from (1, 2, ...).
-	 * @return {Array}  returns a flat array that represents the requested column.
-	 */
+	* Reads a column from the sheet.
+	* @param  {string} which The column to read (eg 'A', 'B', ...).
+	* @param  {string} [start='1'] The row to start reading from (1, 2, ...).
+	* @return {Array}  returns a flat array that represents the requested column.
+	*/
 	column(which, start = '1') {
 		const [x, y] = this.__coordinates(which + start);
 		const results = [];
@@ -94,11 +101,11 @@ export default class Sheet {
 	}
 
 	/**
-	 * Read a row from the sheet.
-	 * @param  {string} which The row to read.
-	 * @param  {string} start The column to start from (defaults to the first column 'A').
-	 * @return {Array}  returns a flat array that represents the requested row.
-	 */
+	* Read a row from the sheet.
+	* @param  {string} which The row to read.
+	* @param  {string} start The column to start from (defaults to the first column 'A').
+	* @return {Array}  returns a flat array that represents the requested row.
+	*/
 	row(which, start = 'A') {
 		const [x, y] = this.__coordinates(start + which);
 
@@ -110,12 +117,28 @@ export default class Sheet {
 	}
 
 	/**
-	 * Convert a coordinate from the A1 notation to [x,y] notation.
-	 * @private
-	 * @param  {string} coord The A1 notation of the coordinates.
-	 * @return {Object} An object with the X property pointing to the X coordinate,
-	 * and the Y property pointing to the Y coordinate.
-	 */
+	* Read a set of rows from the sheet.
+	* @param {string} start The row to start the read from.
+	* @param {number} max The maximum amount of rows to read (or -1 to read all).
+	* @type {String}
+	*/
+	rows(start = 1, max = -1) {
+		const [x, y] = this.__coordinates(`A${start}`);
+
+		if (y >= this.data.length) {
+			return [];
+		}
+
+		return this.data.filter((_, index) => ((index >= y) && (max < 0 || index <= max)) );
+	}
+
+	/**
+	* Convert a coordinate from the A1 notation to [x,y] notation.
+	* @private
+	* @param  {string} coord The A1 notation of the coordinates.
+	* @return {Object} An object with the X property pointing to the X coordinate,
+	* and the Y property pointing to the Y coordinate.
+	*/
 	__coordinates(coord) {
 		if ( ! coord.match(/[a-zA-Z][0-9]+/) ) {
 			throw new Error(`Invalid coordinates given (${coord}).`);
@@ -123,7 +146,7 @@ export default class Sheet {
 
 		const x = coord.charAt().toUpperCase().charCodeAt() - 65; // 65 = 'A'
 		const y = parseInt(coord.substr(1), 10) - 1; // normalize 1 to 0
-
+		
 		return [x, y];
 	}
 }
--- src/Spreadsheet.js
+++ src/Spreadsheet.js
@@ -1,15 +1,15 @@
 import Sheet from './Sheet';
 
 /**
- * A simple wrapper class that exposes the functionality of the Google spreadsheet API (v4).
- */
+* A simple wrapper class that exposes the functionality of the Google spreadsheet API (v4).
+*/
 export default class Spreadsheet {
 	/**
-	 * Create a new spreadsheet.
-	 * @param  {string} key Your Google API key.
-	 * @param  {string} hash Your Spreadsheet hash.
-	 */
-	constructor(hash, key = 'you_forgot_your_api_key') {
+	* Create a new spreadsheet.
+	* @param  {string} key Your Google API key.
+	* @param  {string} hash Your Spreadsheet hash.
+	*/
+	constructor(hash, key = 'GOOGLE_API_KEY') {
 		this.key = key;
 		this.hash = hash;
 		this.ready = false;
@@ -20,39 +20,47 @@ export default class Spreadsheet {
 		this.__cache = new Map();
 		this.__parsing = new Promise((resolve, reject) => {
 			fetch(`https://sheets.googleapis.com/v4/spreadsheets/${this.hash}?key=${this.key}`)
-				.then((response) => {
+			.then((response) => {
+				if (response.ok) {
 					response.json().then(this.__parse.bind(this, resolve));
-				})
-				.catch(reject);
+				} else {
+					reject(response);
+				}
+			})
+			.catch(reject);
 		});
 	}
 
 	/**
-	 * Wait for the current long pending task to finish.
-	 * @return {Promise} The promise that resolves when the Spreadsheet is done loading.
-	 */
+	* Wait for the current long pending task to finish.
+	* @return {Promise} The promise that resolves when the Spreadsheet is done loading.
+	*/
 	wait() {
 		return this.__parsing;
 	}
 
 	/**
-	 * Load a sheet from the spreadsheet.
-	 * @param  {string} name The name of the sheet to request (if left empty, the first sheet is taken).
-	 * @return {Promise}                       [description]
-	 */
+	* Load a sheet from the spreadsheet.
+	* @param  {string} name The name of the sheet to request (if left empty, the first sheet is taken).
+	* @return {Promise}                       [description]
+	*/
 	sheet(name = this.sheets[0]) {
 		if (this.__cache.has(name)) {
 			return this.__cache.get(name);
 		}
 		const promise = new Promise((resolve, reject) => {
 			fetch(`https://sheets.googleapis.com/v4/spreadsheets/${this.hash}/values/${name}?key=${this.key}`)
-				.then((response) => {
+			.then((response) => {
+				if (response.ok) {
 					response.json().then((data) => {
 						const sheet = new Sheet(name, data);
 						resolve(sheet);
 					});
-				})
-				.catch(reject);
+				} else {
+					reject(response);
+				}
+			})
+			.catch(reject);
 		});
 
 		this.__cache.set(name, promise);
@@ -60,11 +68,11 @@ export default class Spreadsheet {
 	}
 
 	/**
-	 * Parse the raw data from the API.
-	 * @private
-	 * @param  {callback} The resolve callback (internally used).
-	 * @param  {Object} raw The raw data returned by the API.
-	 */
+	* Parse the raw data from the API.
+	* @private
+	* @param  {callback} The resolve callback (internally used).
+	* @param  {Object} raw The raw data returned by the API.
+	*/
 	__parse(resolve, raw) {
 		this.title = raw.properties.title;
 		this.locale = raw.properties.locale;
